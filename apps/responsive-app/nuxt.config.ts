a600a603ac2c5f0c4e0b65313df3a7c5f77b1222
guilhemheinrich <guilhem.heinrich@gmail.com>
2025-10-02 12:14:14 +0200

    fix: relative path

--- apps/responsive-app/nuxt.config.ts
+++ apps/responsive-app/nuxt.config.ts
@@ -16,11 +16,11 @@ const config: NuxtConfig = {
         { name: 'description', content: 'A calculator for a Total War: Warhammer 3 Brawl (simple melee combat)' },
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'manifest', href: '/site.webmanifest' }
+        { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: './favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: './favicon-32x32.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: './apple-touch-icon.png' },
+        { rel: 'manifest', href: './site.webmanifest' }
       ]
     }
   },
