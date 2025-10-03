e6c3d222024c3b312175c7de4138fa46dc7a3d23
guilhemheinrich <guilhem.heinrich@gmail.com>
2025-10-03 15:51:40 +0200

    fix: maxEntity calculation for cavalry

--- packages/sdk/src/utils/getMaxEntities.ts
+++ packages/sdk/src/utils/getMaxEntities.ts
@@ -1,7 +1,7 @@
 import { Unit } from '../types';
 
 export function getMaxEntities(unit: Unit) {
-  if (unit.land_unit.num_engines > 0 || unit.land_unit.num_mounts > 0) {
+  if (unit.land_unit.num_engines > 0 && (unit.land_unit.num_engines !== unit.num_men) || (unit.land_unit.num_mounts > 0 && unit.land_unit.num_mounts !== unit.num_men)) {
     return 1;
   }
   return unit.num_men || 1;
